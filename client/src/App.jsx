4fd1d7cb55cf67cfc837966aa4fd9b0eb800a075
Eric M Sandoval <ericmsandoval@gmail.com>
2018-04-18 23:17:54 -0700

    Communication between Login form and server established

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './Login.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
@@ -15,6 +16,16 @@ class App extends React.Component {
     this.user = username;
     console.log('App: ', username);
     console.log('App: ', password);
+    axios.post('/login', {
+      username,
+      password,
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   render() {
--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json()); // for parsing application/json
 app.use(express.static(path.join(__dirname, '../client/public')));
+
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  res.status(200);
+  res.send('This is the server');
+});
+
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));

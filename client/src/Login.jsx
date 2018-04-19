feb7b00a5d9724869a0754f901efd0d5e953c1d2
Eric M Sandoval <ericmsandoval@gmail.com>
2018-04-18 21:54:08 -0700

    Refactor Login to hold form state and handle submit; implements event listeners in Login component

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,54 +1,94 @@
 import React from 'react';
 import { Button, Form, Grid, Header, Message, Segment, Icon } from 'semantic-ui-react';
 
-const Login = () => (
-  <div className="login-container">
-    <Grid
-      textAlign="center"
-      style={{ height: '100%' }}
-      verticalAlign="middle"
-    >
-      <Grid.Column style={{ maxWidth: '40vw' }}>
-        <Header
-          as="h2"
-          color="green"
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const { target: { name, value } } = event;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.username);
+    console.log(this.state.password);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="login-container">
+        <Grid
           textAlign="center"
+          style={{ height: '100%' }}
+          verticalAlign="middle"
         >
-          Welcome to the Futbol App!
-        </Header>
-        <Form>
-          <Segment raised>
-            <Form.Input
-              icon="user"
-              iconPosition="left"
-              placeholder="Username"
-            />
-            <Form.Input
-              icon="lock"
-              iconPosition="left"
-              placeholder="Password"
-              type="password"
-            />
-            <Button fluid color="green" size="large">Login</Button>
-          </Segment>
+          <Grid.Column style={{ maxWidth: '40vw' }}>
+            <Header
+              as="h1"
+              color="green"
+              textAlign="center"
+            >
+              Welcome to the Futbol App!
+            </Header>
+            <Form onSubmit={this.handleSubmit}>
+              <Segment raised>
+                <Form.Input
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Username"
+                  name="username"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                />
+                <Form.Input
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+                <Button
+                  fluid
+                  color="green"
+                  size="large"
+                  type="submit"
+                >
+                Login
+                </Button>
+              </Segment>
+    
+              <Segment raised>
+                <Button fluid size="large" color="facebook">
+                  <Icon name="facebook" /> Sign in with Facebook
+                </Button>
+              </Segment>
+    
+              <Segment raised>
+                <Message>
+                First time here? <a href="">Sign up</a>
+                </Message>
+              </Segment>
+    
+            </Form>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
 
-          <Segment raised>
-            <Button fluid size="large" color="facebook">
-              <Icon name="facebook" /> Sign in with Facebook
-            </Button>
-          </Segment>
-
-          <Segment raised>
-            <Message>
-            First time here? <a href="">Sign up</a>
-            </Message>
-          </Segment>
-
-        </Form>
-      </Grid.Column>
-    </Grid>
-  </div>
-);
 
 export default Login;
 

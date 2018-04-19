49f274fbe096ba4f43baa028861e439a3ff7ea70
Eric M Sandoval <ericmsandoval@gmail.com>
2018-04-18 18:34:04 -0700

    cleanup for readability

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Form, Grid, Header, Image, Message, Segment, Input, Icon } from 'semantic-ui-react';
+import { Button, Form, Grid, Header, Message, Segment, Icon } from 'semantic-ui-react';
 
 const Login = () => (
   <div className="login-container">
@@ -9,25 +9,41 @@ const Login = () => (
       verticalAlign="middle"
     >
       <Grid.Column style={{ maxWidth: '40vw' }}>
-        <Header as="h2" color="green" textAlign="center">
+        <Header
+          as="h2"
+          color="green"
+          textAlign="center"
+        >
           Welcome to the Futbol App!
         </Header>
         <Form>
           <Segment raised>
-            <Form.Input icon="user" iconPosition="left" placeholder="Username" />
-            <Form.Input icon="lock" iconPosition="left" placeholder="Password" type="password" />
+            <Form.Input
+              icon="user"
+              iconPosition="left"
+              placeholder="Username"
+            />
+            <Form.Input
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+            />
             <Button fluid color="green" size="large">Login</Button>
           </Segment>
+
           <Segment raised>
             <Button fluid size="large" color="facebook">
               <Icon name="facebook" /> Sign in with Facebook
             </Button>
           </Segment>
+
           <Segment raised>
             <Message>
             First time here? <a href="">Sign up</a>
             </Message>
           </Segment>
+
         </Form>
       </Grid.Column>
     </Grid>
